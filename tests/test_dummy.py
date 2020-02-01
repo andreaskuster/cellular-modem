2c974f62a90287bd5cb86f4dc9a3e943c8c444b9
Andreas <mail@andreaskuster.ch>
2020-01-31 20:58:53 -0800

    Set up continuous integration and coverage. #2

---
+++ tests/test_dummy.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class Dummy(unittest.TestCase):
+
+    def test_dummy(self):
+        self.assertEqual(42, 7*6)
+
+
+if __name__ == "__main__":
+    # run all tests
+    unittest.main()
