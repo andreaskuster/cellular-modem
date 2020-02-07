e1bf200572333df30c0775cfc2f679bd7499c6b5
Andreas <mail@andreaskuster.ch>
2020-02-07 15:46:26 -0800

    Add licence header.

--- setup.py
+++ setup.py
@@ -1,3 +1,34 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+"""
+MIT License
+
+Copyright (c) 2020 cellular-modem, Andreas Kuster
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+__author__ = "Andreas Kuster"
+__copyright__ = "Copyright 2020, cellular-modem, Andreas Kuster"
+__license__ = "MIT"
+
 import setuptools
 
 with open("README.md", "r") as fh:
@@ -14,6 +45,10 @@ setuptools.setup(
     url="https://github.com/andreaskuster/cellular-modem",
     packages=setuptools.find_packages(),
     classifiers=[
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
@@ -24,5 +59,5 @@ setuptools.setup(
         "Development Status :: 3 - Alpha",
         "Natural Language :: English"
     ],
-    python_requires='>=3.4',
+    python_requires='>=3.0',
 )
