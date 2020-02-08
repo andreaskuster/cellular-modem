7d0c73e1acd6c7b1833aedabb62355f34dbe7dee
Andreas <mail@andreaskuster.ch>
2020-02-07 16:18:31 -0800

    Add proper superclass initialization scheme for multi-class inheritance. Credits: https://rhettinger.wordpress.com/2011/05/26/super-considered-super/

--- cellular_modem/abstract_sms.py
+++ cellular_modem/abstract_sms.py
@@ -3,6 +3,9 @@ from abc import ABC, abstractmethod
 
 class SMS(ABC):
 
+    def __init__(self, **kwds):
+        super().__init__(**kwds)
+
     @abstractmethod
     def send_sms(self):
         raise NotImplementedError()
